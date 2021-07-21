74f05ea5ed6b0fcdf8676b071286f1b8432783da
Bogdan <bogsacha@gmail.com>
2021-07-21 11:59:20 +0200

    add images

--- src/ts/language.ts
+++ src/ts/language.ts
@@ -1,6 +1,6 @@
 // en handle
 
 let en = document.getElementById('en')!;
-let css = 'color: var(--hoover-primary);text-decoration: underline; pointer-events: none';
+let css = 'color: var(--hoover-primary);text-decoration: none; pointer-events: none';
 
 en.style.cssText += css;
