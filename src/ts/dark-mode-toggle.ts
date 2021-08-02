b914eb8e3dcfe0868fe494bcb008d973065cb0e4
Bogdan <bogsacha@gmail.com>
2021-08-02 16:10:35 +0200

    console message

--- src/ts/dark-mode-toggle.ts
+++ src/ts/dark-mode-toggle.ts
@@ -1,13 +1,40 @@
+export {};
+declare global {
+  interface Window {
+    variables: any;
+  }
+}
+
+const displayConsole = (color: string, backgroundColor: string) => {
+  console.clear();
+  const message = window?.variables?.messageConsole || 'Nice Try!';
+  console.log(
+    `%c ${message}`,
+    `
+    color: ${color};
+    background-color: ${backgroundColor};
+    border-radius: 8px;
+    padding: 6px;
+    font-weight: normal;
+    font-family: roobert;
+    font-variation-settings: 'wght' 500;
+    font-size: 18px;
+    `
+  );
+};
+
 const body = document.body;
 
 const theme = localStorage.getItem('theme');
 const toggle = <HTMLInputElement>document.getElementById('toggle-dark-mode');
 
 let isThemeLight = theme === 'light'; // default theme is dark
-if (isThemeLight) { 
+if (isThemeLight) {
   body.classList.replace('dark', 'light');
+  displayConsole('#0015ef', '#fff');
 } else {
   toggle.checked = true;
+  displayConsole('#cab6ff', '#000');
 }
 
 const changeThemeColor = () => {
@@ -16,11 +43,13 @@ const changeThemeColor = () => {
     body.classList.replace('light', 'dark');
     toggle.checked = true;
     isThemeLight = false;
+    displayConsole('#cab6ff', '#000');
   } else {
     localStorage.setItem('theme', 'light');
     body.classList.replace('dark', 'light');
     toggle.checked = false;
     isThemeLight = true;
+    displayConsole('#0015ef', '#fff');
   }
 };
 
