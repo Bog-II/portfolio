b058e2aa2dd5e54356767fff1fba74c3ad6862a1
Bogdan <bogsacha@gmail.com>
2021-08-24 00:36:38 +0200

    refactoring the result

--- src/ts/footer.ts
+++ src/ts/footer.ts
@@ -1,4 +1,4 @@
 const footer = document.getElementById('footer')!;
 
-const currentYear = new Date().getFullYear();
-footer.innerText = '© ' + currentYear.toString() + ' Benoit Bogdan Sachart';
+const currentYear = new Date().getFullYear().toString();
+footer.innerText = `© ${currentYear}, Benoit Bogdan Sachart`;
