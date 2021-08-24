68aec109e35748962e1544ce05fb9ab5c6a91302
Bogdan <bogsacha@gmail.com>
2021-08-24 16:12:37 +0200

    remove language

--- src/ts/language.ts
+++
@@ -1,6 +0,0 @@
-// en handle
-
-let en = document.getElementById('en')!;
-let css = 'color: var(--hoover-primary);text-decoration: none; pointer-events: none; display: inline-block;transform: skew(0, 8deg)';
-
-en.style.cssText += css;
